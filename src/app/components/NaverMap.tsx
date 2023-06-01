e3e635db679c5a9a17ac2e4ffa237ce4af47c662
DongHyeonWon <dhe7700@naver.com>
2023-06-01 13:14:03 +0900

    feat: 위치 설정 안할 경우 default 위치 설정

--- src/app/components/NaverMap.tsx
+++ src/app/components/NaverMap.tsx
@@ -3,56 +3,74 @@ import { useEffect, useRef, useState } from "react";
 import mapMark from "../api/mapMark";
 
 export default function NaverMap() {
+  const mapElement = useRef<HTMLElement | null | any>(null);
+
+  const [myLocation, setMyLocation] = useState<
+    { latitude: number; longitude: number } | string
+  >("");
+
   const success = (position: GeolocationPosition) => {
     if (!mapElement.current || !naver) return;
-    const mapLocation = new naver.maps.LatLng(
-      position.coords.latitude,
-      position.coords.longitude
-    );
-    const drugstoreLocation = mapMark(
-      position.coords.latitude,
-      position.coords.longitude
-    );
-    //네이버 지도 옵션 선택
-    const mapOptions = {
-      center: mapLocation,
-      zoom: 16,
-      zoomControl: true,
-      zoomControlOptions: {
-        position: naver.maps.Position.TOP_RIGHT,
-      },
-    };
-    const map = new naver.maps.Map(mapElement.current, mapOptions);
-    //지도상에 핀 표시 할 부분
-    drugstoreLocation.then((obj) => {
-      Object.values(obj).forEach((index) => {
-        const aroundDrugstore = new naver.maps.LatLng(
-          index.drugstore_lat,
-          index.drugstore_lng
-        );
-        new naver.maps.Marker({
-          position: aroundDrugstore,
-          map: map,
-        });
-      });
-    });
-
-    new naver.maps.Marker({
-      position: mapLocation,
-      map: map,
+    setMyLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
     });
   };
-  const mapElement = useRef(null);
+
+  const error = () => {
+    setMyLocation({ latitude: 37.5666103, longitude: 126.9783882 });
+  };
 
   useEffect(() => {
     const { naver } = window;
     //현재 위치 불러오기
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success);
+      navigator.geolocation.getCurrentPosition(success, error);
     }
   }, []);
+
+  useEffect(() => {
+    if (typeof myLocation !== "string") {
+      const mapLocation = new naver.maps.LatLng(
+        myLocation.latitude,
+        myLocation.longitude
+      );
+      const drugstoreLocation = mapMark(
+        myLocation.latitude,
+        myLocation.longitude
+      );
+      //네이버 지도 옵션 선택
+      const mapOptions = {
+        center: mapLocation,
+        zoom: 16,
+        zoomControl: true,
+        zoomControlOptions: {
+          position: naver.maps.Position.TOP_RIGHT,
+        },
+      };
+      const map = new naver.maps.Map(mapElement.current, mapOptions);
+      //지도상에 핀 표시 할 부분
+      drugstoreLocation.then((obj) => {
+        Object.values(obj).forEach((index) => {
+          const aroundDrugstore = new naver.maps.LatLng(
+            index.drugstore_lat,
+            index.drugstore_lng
+          );
+          new naver.maps.Marker({
+            position: aroundDrugstore,
+            map: map,
+          });
+        });
+      });
+
+      new naver.maps.Marker({
+        position: mapLocation,
+        map: map,
+      });
+    }
+  }, [myLocation]);
   return (
-    <div className="container	mx-auto">
+    <div className="container mx-auto">
       <div
         ref={mapElement}
         className="w-[40rem] h-[40rem] rounded-2xl border"
